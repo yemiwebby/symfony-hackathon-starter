528a6bd4c946941ddc607f76f5847d68a5b4347b
yemiwebby <yemiwebby@gmail.com>
2018-10-14 04:50:01 +0100

    added entity manager

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -9,10 +9,42 @@
 namespace App\Controller;
 
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccountController extends AbstractController
 {
-    
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
 
+    /**
+     * @var \Doctrine\Common\Persistence\ObjectRepository
+     */
+    private $userRepository;
+
+    /**
+     * AccountController constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->userRepository = $entityManager->getRepository('App:User');
+    }
+
+    public function deleteAccount()
+    {
+
+    }
+
+
+    function persistObject() {
+
+    }
+
+    function removeObject() {
+
+    }
 }
