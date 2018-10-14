d28f1586864ff273ca8238728647d8340508a6c0
yemiwebby <yemiwebby@gmail.com>
2018-10-14 04:40:45 +0100

    created account controller

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: webby
+ * Date: 14/10/2018
+ * Time: 4:40 AM
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AccountController extends AbstractController
+{
+    
+
+}
