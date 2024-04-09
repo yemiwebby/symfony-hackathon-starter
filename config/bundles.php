f4333f12f7092a84913ee12b8c6ee7fbdaa26aa8
Joseph Udonsak <joseph.udonsak@ibn.com.ng>
2024-04-09 13:12:00 +0100

    Update Symfony dependencies to version 7

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     cspoo\Swiftmailer\MailgunBundle\cspooSwiftmailerMailgunBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
